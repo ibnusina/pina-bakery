ba22ec211101d30e685e6d3d1f245a8d0e028429
ibnu <ibnu.sina009@gmail.com>
2025-01-26 17:27:02 +0700

    integrate tailwind

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-green-400">
+      <Text className="text-red-800">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
