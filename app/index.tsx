723e211cce51ae04db6c92df1ada15822682b0cd
ibnu <ibnu.sina009@gmail.com>
2025-02-07 16:44:23 +0700

    update image

--- app/index.tsx
+++ app/index.tsx
@@ -9,19 +9,13 @@ import {
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "@react-navigation/native";
-import {
-  Search,
-  ShoppingBag,
-  ReceiptText,
-  CakeSlice,
-  Donut,
-  Croissant,
-} from "lucide-react-native";
+import { ShoppingBag } from "lucide-react-native";
 import MenuCard from "@/components/MenuCard";
 import CategoryRadioItem from "@/components/CategoryRadioItem";
 import CategoryRadio from "@/components/CategoryRadio";
 import { categories, FoodCategory } from "@/constants/Category";
 import { products } from "@/constants/Product";
+import logo from "@/assets/images/logo.png";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
@@ -31,9 +25,18 @@ export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="flex-row items-center justify-between px-4 py-2">
-        <TouchableOpacity>
-          <Text className="text-2xl">≡</Text>
-        </TouchableOpacity>
+        <View className="flex-row flex-shrink flex-2 items-center justify-start gap-1">
+          <View className="h-[32px] w-[32px] rounded-2xl bg-blue-950">
+            <Image
+              source={logo}
+              className="absolute h-full w-full object-contain"
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-lg font-bold">Pina Bakery</Text>
+        </View>
+
         <TouchableOpacity
           className="flex-row items-center bg-orange-500 rounded-full px-3 py-1"
           onPress={() => navigation.navigate("Cart")}
@@ -42,6 +45,7 @@ export default function HomeScreen() {
           <Text className="text-white ml-1">{cartItemsCount}</Text>
         </TouchableOpacity>
       </View>
+      <View className="h-[1px] bg-gray-300"></View>
 
       {/* <View className="px-4 py-2">
         <View className="flex-row items-center bg-gray-100 rounded-full px-4 py-2">
