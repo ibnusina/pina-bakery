8cf433323be297e7c39d28c3dfb963dcb0615693
ibnu <ibnu.sina009@gmail.com>
2025-02-25 14:17:56 +0700

    add analytics

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,14 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import ReactGA from "react-ga";
+import { useEffect } from "react";
+
+ReactGA.initialize("G-49LQ6M85WH");
 
 export default function RootLayout() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
